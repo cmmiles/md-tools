083852631361af134bba26e29404adc553945909
cmmiles <c.miles.1@warwick.ac.uk>
2023-09-15 18:12:39 +0100

    Shell script used to build docs ready for GitHub Pages.

--- docs4git.sh
+++ docs4git.sh
@@ -1,4 +1,5 @@
 # Builds Rustdocs for deployment to GitHub Pages
+# Credit: https://dev.to/deciduously
 cargo doc --no-deps
 rm -rf ./docs
 echo "<meta http-equiv=\"refresh\" content=\"0; url=md_tools\">" > target/doc/index.html
